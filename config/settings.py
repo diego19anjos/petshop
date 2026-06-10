1d17b8e68d81a28867861615342592ea532ef69f
diego19anjos <diego19anjos@gmail.com>
2026-06-10 20:02:17 -0300

    degub false

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='chave-secreta-de-desenvolvimento-super-segura')
 
 # Desativa o modo de depuração na internet para ninguém ver seus erros de código
-DEBUG = config('DEBUG', default=True,cast=bool)
+DEBUG = config('DEBUG', default=False,cast=bool)
 
 # Permite que o link gerado pelo Railway acesse o projeto
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
